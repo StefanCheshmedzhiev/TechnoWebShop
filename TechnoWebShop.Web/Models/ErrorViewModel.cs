d7ec367fe1f457ab124962c469db2827e2dfba96
StefanCheshmedzhiev <s.cheshmedzhiev@abv.bg>
2021-07-03 17:51:11 +0300

    Add TechnoWebShop Web layes and configuring the Setup class

---
+++ TechnoWebShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechnoWebShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
