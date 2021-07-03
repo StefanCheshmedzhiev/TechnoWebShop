193ac3b537d3d3feecd6605adbe428654eab29fb
StefanCheshmedzhiev <s.cheshmedzhiev@abv.bg>
2021-07-03 17:49:15 +0300

    Add IdentityDbContext and Data layer

---
+++ TechnoWebShop.Data/TechnoWebShopDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TechnoWebShop.Data.Models;
+
+namespace TechnoWebShop.Data
+{
+    public class TechnoWebShopDbContext : IdentityDbContext<WebShopUser, IdentityRole, string>
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+
+        public DbSet<Receipt> Receipts { get; set; }
+        public TechnoWebShopDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        
+    }
+}
